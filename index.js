2fe70e3585703ec568723bfd39a15e80d67848b8
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-27 17:27:52 +0600

    get single Trainers by id added

--- index.js
+++ index.js
@@ -69,6 +69,16 @@ async function run() {
       res.send(result);
     });
 
+    // Get Single Trainers By Id
+    app.get('/trainers/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await trainersLetterCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
