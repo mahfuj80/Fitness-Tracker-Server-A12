d4ff9e54c628480cd80af166581627d60717e263
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-28 11:14:30 +0600

    token verify middleware Created

--- index.js
+++ index.js
@@ -48,6 +48,22 @@ async function run() {
       res.send({ token });
     });
 
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verified token', req.headers.authorization);
+      if (!req.headers?.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     // conform Server is Running
     app.get('/', (req, res) => {
       res.send('Fitness Tracker is running...');
